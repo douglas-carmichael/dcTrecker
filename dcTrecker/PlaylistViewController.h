958bd23bee8a172c30c82bc5d9d4070881367a66
Douglas Carmichael <dcarmich@Douglass-Mac-Pro.local>
2015-04-13 04:22:16 -0500

    Initial table support.

---
+++ dcTrecker/PlaylistViewController.h
@@ -0,0 +1,20 @@
+//
+//  PlaylistViewController.h
+//  dcTrecker
+//
+//  Created by Douglas Carmichael on 4/13/15.
+//  Copyright (c) 2015 Douglas Carmichael. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PlaylistViewController : NSViewController
+
+{
+    IBOutlet NSTableView *playlistTable;
+    
+}
+-(IBAction)addToPlaylist:(id)sender;
+-(IBAction)removeFromPlaylist:(id)sender;
+
+@end
---
+++ dcTrecker/PlaylistViewController.m
@@ -0,0 +1,31 @@
+//
+//  PlaylistViewController.m
+//  dcTrecker
+//
+//  Created by Douglas Carmichael on 4/13/15.
+//  Copyright (c) 2015 Douglas Carmichael. All rights reserved.
+//
+
+#import "PlaylistViewController.h"
+
+@interface PlaylistViewController ()
+
+@end
+
+@implementation PlaylistViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+-(IBAction)addToPlaylist:(id)sender
+{
+    NSLog(@"Add to playlist not implemented yet!");
+}
+
+-(IBAction)removeFromPlaylist:(id)sender
+{
+    NSLog(@"Remove from playlist not implemented yet!");
+}
+@end
